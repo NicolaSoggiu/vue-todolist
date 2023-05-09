49d8fb69ba4f1841f5703f04d990f09f430ed605
NicolaSoggiu <Nicola.soggiu.work@gmail.com>
2023-05-09 15:42:29 +0200

    add "todos" in html

--- js/script-vue.js
+++ js/script-vue.js
@@ -1,6 +1,30 @@
 const app = Vue.createApp({
   data() {
-    return {};
+    return {
+      newTodo: "",
+      todos: [
+        {
+          text: "fare la spesa",
+          done: "<i class='bi bi-x'></i>",
+        },
+        {
+          text: "andare dal meccanico",
+          done: "<i class='bi bi-x'></i>",
+        },
+        {
+          text: "ritirare il pacco",
+          done: "<i class='bi bi-x'></i>",
+        },
+        {
+          text: "passare dal medico",
+          done: "<i class='bi bi-x'></i>",
+        },
+        {
+          text: "andare a correre",
+          done: "<i class='bi bi-x'></i>",
+        },
+      ],
+    };
   },
   methods: {},
 });
